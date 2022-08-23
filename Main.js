488a7bf5a3ead1d1b72265db8a7ab791496a19b5
LiamHepworth <liammoses1@gmail.com>
2022-08-23 22:45:48 +0100

    continue building mobile homepage

---
+++ Main.js
@@ -0,0 +1,13 @@
+console.log('js linked sucessfully')
+
+let pageMainSection = document.querySelector('#page-main-section');
+let headerSection = document.querySelector('#header-section');
+const burgerMenuMobile = document.querySelector('.burger-menu');
+
+burgerMenuMobile.addEventListener('click', function(){
+    
+});
+
+function clearPage(containerName){
+    containerName.innerHTML = '';
+}
