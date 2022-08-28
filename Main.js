d3168782d2242bc7c0c7a5f0f1530eed88935370
LiamHepworth <liammoses1@gmail.com>
2022-08-28 12:32:30 +0100

    Functional Burger Menu//CSS tweaks
    Making the burger menu functional; appearing, disappearing, changing icon from a menu icon to a cross and back when menu is opened. Links to be added at a later date once other pages have been constructed.
    
    Some small tweaks to css also to make type styling a little bit more modular, with the introduction of header and sub-header classes which can be re-used in differing contexts.

--- Main.js
+++ Main.js
@@ -1,12 +1,41 @@
-console.log('js linked sucessfully')
-
 let pageMainSection = document.querySelector('#page-main-section');
 let headerSection = document.querySelector('#header-section');
 const burgerMenuMobile = document.querySelector('.burger-menu');
 
-burgerMenuMobile.addEventListener('click', function(){
+
+function showMenu(){
+        clearPage(pageMainSection);
     
-});
+        let menuList = document.createElement('ul');
+        let menuListNames = ['HOME', 'WORK', 'ABOUT', 'CONTACT'];
+        let menuListItems = [];
+    
+        for(let i = 0; i < 4; i++){
+            menuListItems[i] = document.createElement('li');
+            menuListItems[i].innerText = menuListNames[i];
+            menuListItems[i].classList.add('basic-text', 'sub-header', 'menu-list-items');
+            menuList.appendChild(menuListItems[i])
+        }
+        pageMainSection.appendChild(menuList);
+        menuList.classList.add('menu-list', 'vertical-center') 
+    
+        burgerMenuMobile.innerText = 'close';
+}
+
+function hideMenu(){
+        clearPage(pageMainSection);
+        burgerMenuMobile.innerText = 'menu';
+}
+
+burgerMenuMobile.addEventListener('click', function(){
+    if(burgerMenuMobile.innerText == 'close'){
+        hideMenu();
+        console.log('hiding Menu');
+    } else if(burgerMenuMobile.innerText == 'menu'){
+        showMenu();
+        console.log('showing Menu');
+    }
+})
 
 function clearPage(containerName){
     containerName.innerHTML = '';
