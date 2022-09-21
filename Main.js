4a97e1e87956d59b3659bcbd758af80e6f7d9edc
LiamHepworth <liammoses1@gmail.com>
2022-09-21 20:30:54 +0100

    Testing video implementation

--- Main.js
+++ Main.js
@@ -141,11 +141,11 @@ let helperFunc = {
 }
 
 class Project {
-    constructor(name, type, images, video, embeddedContent, description, thumbnail) {
+    constructor(name, type, images, videos, embeddedContent, description, thumbnail) {
         this.name = name;
         this.type = type;
         this.images = images;
-        this.video = video;
+        this.videos = videos;
         this.embeddedContent = embeddedContent;
         this.description = description;
         this.thumbnail = thumbnail;
@@ -160,13 +160,44 @@ class Project {
 
     carouselCreation(container){
 
-        let images = [];
+        // if(this.videos !== null){
 
-        for(let i = 0; i < this.images.length; i++){
-            images[i] = new Image();
-            images[i].src = this.images[i];
-            container.appendChild(images[i]);
-        };
+        //     if(this.videos.length > 1){
+        //         console.log('more than one')
+
+                // this.videos.forEach(() => {
+                //     // container.appendChild(video);
+                // });
+
+                // for(let i = 0; i <this.videos.length; i++){
+                //     console.log('working');
+
+                //     let video = document.createElement('video');
+                //     let source = document.createElement('source');
+                //     helperFunc.setAttributes(source, {src: this.videos, type:'video/mp4'});
+                //     video.appendChild(source);
+                //     video.classList.add('videos');
+                // }
+
+
+            // } else {
+            //     console.log('less than one')
+            //         let video = document.createElement('video');
+            //         let source = document.createElement('source');
+            //         helperFunc.setAttributes(source, {src: this.videos, type:'video/mp4', alt: 'project video'});
+            //         video.appendChild(source);
+            //         video.classList.add('videos');
+            //         video.appendChild(source);
+            // };
+        // } else {
+            let imageStore = [];
+    
+            for(let i = 0; i < this.images.length; i++){
+                imageStore[i] = new Image();
+                imageStore[i].src = this.images[i];
+                container.appendChild(imageStore[i]);
+            }; 
+        // }
     };
 
     pushProjectPageHistory(){
@@ -179,8 +210,8 @@ class Project {
 let projectList = [
     new Project('UNREAL TEST', 'poster', ['projects/unreal/image01.png', 'projects/unreal/image02.png'], null, null, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, ex in scelerisque placerat, velit dui ultricies ipsum, viverra facilisis elit ex vitae lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis auctor sem, ac elementum quam. Fusce vitae ante dapibus, tempus erat in, hendrerit nibh. Suspendisse fringilla pellentesque elit, a tempus augue fringilla non. Aliquam sodales, nisl sed malesuada laoreet, libero ligula scelerisque nibh, in efficitur orci ex sed lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Vestibulum aliquet vestibulum metus eget blandit. Curabitur sodales sit amet nisl ut fermentum. Cras tristique justo diam, nec eleifend ex cursus feugiat. Nam egestas velit lectus, ac ullamcorper tortor lobortis non.', 'projects/unreal/image01.png'),
     new Project('severe', 'poster', ['projects/severe/image01.jpg'], null, null, null,'projects/severe/image01.jpg'),
-    new Project('Exile Corp HoloDisk Reader', 'Animated Poster (2022)', null, 'projects/video-test/test01.mp4', null, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, ex in scelerisque placerat, velit dui ultricies ipsum, viverra facilisis elit ex vitae', 'projects/unreal/image01.png')
-];
+    new Project('Exile Corp HoloDisk Reader', 'Animated Poster (2022)', null, ['projects/video-test/test01.mp4'], null, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, ex in scelerisque placerat, velit dui ultricies ipsum, viverra facilisis elit ex vitae', 'projects/unreal/image01.png')
+]; 
 
 let displayPage = {
     home: function displayHomePage(){
@@ -352,4 +383,4 @@ let displayPage = {
     }
 };
 
-displayPage.project(1);
+// displayPage.project(2);
