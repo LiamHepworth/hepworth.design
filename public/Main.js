8e1922e83d362d27dfd552e37ff82390b0de00ba
LiamHepworth <liammoses1@gmail.com>
2022-12-17 13:36:48 +0000

    Working on Custom cursor

--- public/Main.js
+++ public/Main.js
@@ -8,6 +8,16 @@ const pageHeader = document.querySelector('.header');
 const burgerMenuMobile = document.querySelector('#burger-menu');
 const menuContainer = document.createElement('nav');
 const bottomNavBar = document.querySelector('.bar.bottom-bar');
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+    let x = e.pageX;
+    let y = e.pageY;
+    // cursor.style.left = (x - 15) + "px";
+    // cursor.style.top = (y - 15) + "px";
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+});
 
 let util = {
     resetPage: (containers) => {
