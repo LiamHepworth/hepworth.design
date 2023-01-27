35312b4dde2288635fb474f0285ad74ef72e9d6e
LiamHepworth <liammoses1@gmail.com>
2023-01-27 19:59:31 +0000

    Pre-THREE implementation
    More new modules + webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { util } from "./app/utilities";
+import { projectList } from "./app/projectList";
 import { Page, Section } from "./app/pageSections";
-import { Project } from "./app/projectSetup";
 
 const fullPage = document.querySelector('html')
 const pageBody = document.querySelector('body');
@@ -216,12 +216,6 @@ navCreation();
         };
     });
 })();
-
-const projectList = [
-    new Project('UNREAL TEST', 'poster', ['/src/assets/project-images/unreal/image01.png', '/src/assets/project-images/unreal/image02.png'], null, null, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, ex in scelerisque placerat, velit dui ultricies ipsum, viverra facilisis elit ex vitae lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis auctor sem, ac elementum quam. Fusce vitae ante dapibus, tempus erat in, hendrerit nibh. Suspendisse fringilla pellentesque elit, a tempus augue fringilla non. Aliquam sodales, nisl sed malesuada laoreet, libero ligula scelerisque nibh, in efficitur orci ex sed lacus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam erat volutpat. Vestibulum aliquet vestibulum metus eget blandit. Curabitur sodales sit amet nisl ut fermentum. Cras tristique justo diam, nec eleifend ex cursus feugiat. Nam egestas velit lectus, ac ullamcorper tortor lobortis non.', '/src/assets/project-images/unreal/image01.png'),
-    new Project('SEVERE', 'poster', ['/src/assets/project-images/severe/image01.jpg', '/src/assets/project-images/severe/image01.jpg'], null, null, 'Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, ex in scelerisque placerat, velit dui ultricies ipsum, viverra facilisis ', '/src/assets/project-images/severe/image01.jpg'),
-    new Project('Exile Corp HoloDisk Reader', 'Animated Poster (2022)', null, ['/src/assets/project-images/video-test/Comp 2.mp4'], null, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras imperdiet, ex in scelerisque placerat, velit dui ultricies ipsum, viverra facilisis elit ex vitae', '/src/assets/project-images/unreal/image01.png'),
-]; 
 
 let displayPage = {
     home: () => {
