d189f5c25eceeff75f6cce7707c0d0e2a426136e
LiamHepworth <Liammoses1@gmail.com>
2023-02-13 11:38:18 +0000

    Model creation loop?

--- src/app/mainScene.js
+++ src/app/mainScene.js
@@ -4,10 +4,26 @@ import * as THREE from 'three';
 //scene
 const scene = new THREE.Scene();
 
+//renderer
+let mainCanvas = util.createEl("canvas", "main-canvas"); //define custom canvas
+export const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: mainCanvas});
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true;
+// document.body.appendChild( renderer.domElement );  //append scene to DOM, included in main file;
+
 //camera
-const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0, 3, 30);
+const camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 3, 50);
 camera.rotation.set(0, 0, 0);
+
+//resize controls
+window.addEventListener( 'resize', onWindowResize, false );
+function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
 
 //geometry and materials
 const cubeGeom = new THREE.BoxGeometry( 6, 6, 6 );
@@ -22,11 +38,13 @@ const cubeMat = new THREE.MeshStandardMaterial( {color: 0x0092bd, side: THREE.Do
         models[i].castShadow = true;
         scene.add(models[i]);
     }
-    
-    models[0].position.set(window.innerWidth/170, 0, 0)
+
+    //FIX - doesn't resize on window.
+    models[0].position.set(window.innerWidth/150, 0, 0)
     models[1].position.set(0, 5, 0)
-    models[2].position.set(-Math.abs(window.innerWidth/170), 0, 0)
-    
+    models[2].position.set(-Math.abs(window.innerWidth/150), 0, 0)
+
+
     //mouseLook event - makes cubes look at cursor
     let plane = new THREE.Plane(new THREE.Vector3(0, 0, 1), -5); //use last control to define intersection distance from object
     let raycaster = new THREE.Raycaster();
@@ -42,11 +60,13 @@ const cubeMat = new THREE.MeshStandardMaterial( {color: 0x0092bd, side: THREE.Do
         for(let i = 0; i < 3; i++){
             models[i].lookAt(pointOfIntersection);
         }
-
     });
 })();
 
 //lighting
+const ambLight = new THREE.AmbientLight(0xffffff, 0.1)
+scene.add(ambLight);
+
 const pointLight = new THREE.PointLight(0xffffff, 1, 50)
 pointLight.position.set(0, 10, 10);
 pointLight.castShadow = true;
@@ -56,13 +76,6 @@ scene.add(pointLight);
 // var clock = new THREE.Clock();
 // var time = 0;
 // var delta = 0;
-
-//renderer
-let mainCanvas = util.createEl("canvas", "main-canvas"); //define custom canvas
-export const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: mainCanvas});
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.shadowMap.enabled = true;
-// document.body.appendChild( renderer.domElement );  //append scene to DOM, included in main file;
 
 //animate loop
 function animate() {
