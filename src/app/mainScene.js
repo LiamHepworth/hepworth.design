fc2a89e27cd8900b62429ceb05592cb2077f8bfc
LiamHepworth <Liammoses1@gmail.com>
2023-02-19 14:04:08 +0000

    Hardcoding position + OrbitControls
    Hardcoding some model positions and adding in orbitcontrols in place of the raycaster effect for mobile devices.

--- src/app/mainScene.js
+++ src/app/mainScene.js
@@ -1,6 +1,7 @@
 import { util } from './utilities';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 //scene
 const scene = new THREE.Scene();
@@ -17,7 +18,14 @@ renderer.toneMappingExposure = 1.7;
 //camera
 const camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.set(0, 3, 50);
-camera.rotation.set(0, 0, 0);
+// camera.rotation.set(0, 0, 0);
+
+//controls
+let controls = new OrbitControls( camera, renderer.domElement );
+controls.enabled = false;
+controls.minPolarAngle = 0.8;
+controls.maxPolarAngle = Math.PI/1.5;
+// controls.update();
 
 //loading model and adding to scene
 const loader = new GLTFLoader();
@@ -51,6 +59,15 @@ function createMat(){
     return mat;
 }
 
+function applyMat(modelNum, textureName){
+    models[modelNum].traverse(child => {
+        if(child.name == 'Screen_01'){
+            child.material = createMat();
+            child.material.emissiveMap = textureName;
+        }
+    })
+}
+
 let designVideoTexture = createVidTexture('./assets/THREE-Videos/design.mp4');
 let threeDVideoTexture = createVidTexture('./assets/THREE-Videos/3D.mp4');
 let codeVideoTexture = createVidTexture('./assets/THREE-Videos/Code.mp4'); 
@@ -61,35 +78,18 @@ loader.load(
 
         monitorModel = gltfScene.scene;
 
-        models.push(monitorModel)
-        models.push(monitorModel.clone())
-        models.push(monitorModel.clone())
+        models.push(monitorModel);
+        models.push(monitorModel.clone());
+        models.push(monitorModel.clone());
 
         models[0].position.y = 6;
         models[1].position.y = -2;
         models[2].position.y = -2;
         modelBreakPoints();
 
-        models[0].traverse(child => {
-            if(child.name == 'Screen_01'){
-                child.material = createMat();
-                child.material.emissiveMap = threeDVideoTexture;
-            }
-        })
-
-        models[1].traverse(child => {
-            if(child.name == 'Screen_01'){
-                child.material = createMat();
-                child.material.emissiveMap = codeVideoTexture;
-            }
-        })
-
-        models[2].traverse(child => {
-            if(child.name == 'Screen_01'){
-                child.material = createMat();
-                child.material.emissiveMap = designVideoTexture;
-            }
-        })
+        applyMat(0, threeDVideoTexture);
+        applyMat(1, codeVideoTexture);
+        applyMat(2, designVideoTexture);
 
         scene.add(models[0], models[1], models[2]);
         return models;
@@ -113,32 +113,38 @@ function modelBreakPoints(){
             models[2].position.x = - window.innerWidth/160; 
         }
         else if(window.innerWidth > 1080){
-            models[1].visible = true;
-            models[2].visible = true;
-            models[0].position.y = 6;
-    
-            models.forEach(model => model.scale.set(0.9, 0.9, 0.9))
+         models.forEach(model => model.scale.set(0.9, 0.9, 0.9))
             models[1].position.x = window.innerWidth/150;
             models[2].position.x = - window.innerWidth/150; 
         }
         else if(window.innerWidth > 800 && window.innerHeight > 700){
-            models[1].visible = true;
-            models[2].visible = true;
-            models[0].position.y = 6;
-    
-            models.forEach(model => model.scale.set(0.8, 0.8, 0.8))
+         models.forEach(model => model.scale.set(0.8, 0.8, 0.8))
             models[1].position.x = window.innerWidth/150;
             models[2].position.x = - window.innerWidth/150; 
         }
         else if(window.innerWidth > 600){
-            models[1].visible = false;
-            models[2].visible = false;
-            models[0].position.y = 3;
-    
-            models[0].scale.set(1, 1, 1);
+            models[1].position.x = window.innerWidth/120;
+            models[2].position.x = - window.innerWidth/120; 
+            models.forEach(model => model.scale.set(0.8, 0.8, 0.8))
+
+            models[0].rotation.set(0.05, 0.32, 0.02);
+            models[0].position.y = 5;
+            models[1].rotation.set(-0.05, 0.32, -0.17);
+            models[1].position.y = -4;
+            models[2].rotation.set(0.23, -0.32, 0.02);
+            models[2].position.y = -3;
         }
-        else if(window.innerWidth < 600){
-            models[0].scale.set(.8, .8, .8);
+        else{
+            models.forEach(model => model.scale.set(0.7, 0.7, 0.7))
+            models[1].position.x = window.innerWidth/100;
+            models[2].position.x = - window.innerWidth/100; 
+
+            models[0].rotation.set(0.05, 0.32, 0.02);
+            models[0].position.y = 4;
+            models[1].rotation.set(-0.05, 0.32, -0.17);
+            models[1].position.y = -3;
+            models[2].rotation.set(0.23, -0.32, 0.02);
+            models[2].position.y = -2;
         }
 }
 
@@ -188,9 +194,23 @@ function animate() {
 
     //if statement to check whether model has loaded before applying rotation
     if(monitorModel){
-        models[0].lookAt(pointOfIntersection);
-        models[1].lookAt(pointOfIntersection);
-        models[2].lookAt(pointOfIntersection);
+        if(window.innerWidth < 1080){
+            // models[0].lookAt(0, 3, 50);
+            // models[0].rotation.x += 0.0001
+            // models[0].rotation.y += 0.001
+            scene.rotation.y += 0.001
+            controls.enabled = true;
+            controls.enablePan = false;
+            controls.update()
+        } else {
+            scene.rotation.y = 0;
+            models[0].lookAt(pointOfIntersection);
+            models[1].lookAt(pointOfIntersection);
+            models[2].lookAt(pointOfIntersection);
+            camera.position.set(0, 3, 50);
+            camera.rotation.set(0, 0, 0);
+            controls.enabled = false;
+        }
     }
 
     renderer.render( scene, camera );
