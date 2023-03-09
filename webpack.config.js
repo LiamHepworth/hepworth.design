0bf7ae009baec2ebba3743eecfac7c44f48c88e0
LiamHepworth <Liammoses1@gmail.com>
2023-03-09 15:16:18 +0000

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ const path = require("path")
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "docs")
