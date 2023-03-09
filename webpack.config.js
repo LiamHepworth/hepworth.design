07ca26c19ae055e137af0689b9ddcc46479dc8ee
LiamHepworth <Liammoses1@gmail.com>
2023-03-09 15:26:22 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     module: {
         rules: [
           {
-            test: /\.css$/i,
+            test: /\.css$/,
             use: ["style-loader", "css-loader"],
           },
         //   {
